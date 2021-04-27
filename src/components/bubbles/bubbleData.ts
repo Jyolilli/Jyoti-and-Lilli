45f8d924aa94d946d2276721bb859dd2212cca5f
BOZ2323 <l.baaske@sapera.com>
2021-04-27 17:15:36 +0200

    fix/update-import-bubble-image

--- src/components/bubbles/bubbleData.ts
+++ src/components/bubbles/bubbleData.ts
@@ -1,9 +1,21 @@
 const bubbleData = [
     {
-        id: 1,
+      id: 1,
       copy: 'Hey Jyoti, do you remember how we met?',
       text_link: 'Check this for more information',
-      bubble: '../public/images/bubble_s_left.svg'
+      bubble: '/images/bubble_s_left.svg',
+    },
+    {
+      id: 2,
+      copy: 'Oh, yes, July 2020 at Codebar Berlin? We shared one coach and both worked on a React project',
+      text_link: 'Check this for more information',
+      bubble: '/images/bubble_s_right.svg',
+    },
+    {
+      id: 3,
+      copy: 'Next week we will do this workshop on Typescript together. Cannot wait for my brain to finally click when it comes to TS',
+      text_link: 'Check this for more information',
+      bubble: '/images/bubble_s_left.svg',
     },
   ];
   
