928f1df7a36f7b953a23811cd85b14d420bfdd07
BOZ2323 <l.baaske@sapera.com>
2021-05-18 17:13:14 +0200

    fix/some-refactoring

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
 
 // the use of gql turns the query into a proper graphql query
-const ADD_USER = gql`  
-    mutation insert_users_one(
-      $object: users_insert_input! = {name: "Test App Name"}
-      ) {
-      insert_users_one(object: $object) {
-        name
-        id
-      }
+const ADD_USER = gql`
+  mutation insert_users_one(
+    $object: users_insert_input! = { name: "Test App Name" }
+  ) {
+    insert_users_one(object: $object) {
+      name
+      id
     }
-  `;
+  }
+`;
 
 const Comments = () => {
-    useQuery(ADD_USER)
-    return (
-        <div>
-            Comments
-        </div>
-    )
-}
+  const [addUser, { data }] = useMutation(ADD_USER);
 
-export default Comments
+  const handleSubmit = async () => {
+    console.log("data useMutation", data);
+    console.log("addUser", addUser);
+  };
+
+  return (
+    <div>
+      <h1>comments!!</h1>
+      <button onClick={handleSubmit}></button>
+    </div>
+  );
+};
+
+export default Comments;
